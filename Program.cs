7ccf4be2e1f667cf4c5f31777c23c24604c8077b
Brett Bellomy <brettbellomymusic@gmail.com>
2024-08-20 15:50:43 -0500

    completed assignment

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿for (int i = 1; i < 101; i++)
+{
+	if (i % 5 == 0 && i % 3 == 0)
+	{
+        Console.WriteLine($"{i}. FIZZBUZZ");
+    }
+	else if (i % 5 == 0)
+	{
+        Console.WriteLine($"{i}. FIZZ");
+    }
+	else if (i % 3 == 0)
+	{
+        Console.WriteLine($"{i}. BUZZ");
+    }
+	else { Console.WriteLine(i); }
+}
